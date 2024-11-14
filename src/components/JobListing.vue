3dac07ea0dded0b241c4ab3db836ebcdcd927653
PiotrSiemi <siemienkiewiczpiotr@gmail.com>
2024-11-14 15:35:41 +0100

    Final project

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,11 +1,16 @@
 <script setup>
 import { defineProps, ref, computed } from 'vue';
+import { RouterLink } from 'vue-router';
 
 const props = defineProps({
   job: Object
 });
 
 const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+}
 
 const truncatedDescription = computed (() => {
   let description = props.job.description;
@@ -24,7 +29,14 @@ const truncatedDescription = computed (() => {
         <h3 class="text-xl font-bold">{{ job.title }}</h3>
       </div>
 
-      <div class="mb-5">{{ truncatedDescription }}</div>
+      <div class="mb-5">
+        <div>
+          {{ truncatedDescription }}
+        </div>
+        <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+          {{ showFullDescription ? 'Less' : 'More' }}
+        </button>
+      </div>
 
       <h3 class="text-green-500 mb-2">{{  job.salary }} / Year</h3>
 
@@ -32,15 +44,15 @@ const truncatedDescription = computed (() => {
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
-        <a
-          :href="'/job/' + job.id"
+        <RouterLink
+          :to="'/jobs/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
-        </a>
+        </RouterLink>
       </div>
     </div>
   </div>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,15 +1,36 @@
 <script setup>
-import jobData from '@/jobs.json';
+import { RouterLink } from 'vue-router';
 import JobListing from './JobListing.vue';
-import { ref, defineProps } from 'vue';
+import { reactive ,ref, defineProps, onMounted } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import axios from 'axios';
 
-const jobs = ref(jobData);
+//const jobs = ref([]);
+const state = reactive({
+  jobs: [],
+  isLoading: true
+});
+
 defineProps({
   limit: Number,
   showButton: {
     type: Boolean,
-    default: false
+    default: false,
+  },
+});
+
+onMounted(async() => {
+  try {
+    const response = await axios.get('/api/jobs');
+    //jobs.value = response.data;
+    state.jobs = response.data;
+  } catch (error){
+    console.error('Error fetching data', error);
+  } finally {
+    state.isLoading = false;
   }
+
+  
 });
 
 </script>
@@ -21,16 +42,21 @@ defineProps({
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse jobs
       </h2>
-      <div class="grid grid-cols1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/>
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <!-- Show job listing when done loading -->
+      <div v-else class="grid grid-cols1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
       </div>
     </div>
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
 </template>
